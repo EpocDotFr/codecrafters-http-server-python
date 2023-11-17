26ff5db04b6b46c60e2223b7c4ebca4695ddb5ae
EpocDotFr <contact@epoc.fr>
2023-11-17 11:16:00 +0100

    Stage 2

--- app/handler.py
+++ app/handler.py
@@ -1,17 +1,49 @@
 from socketserver import StreamRequestHandler
+from typing import Optional, Tuple, Dict
 from app.server import HTTPServer
 
 
 class HTTPHandler(StreamRequestHandler):
     server: HTTPServer
 
-    def receive(self):
-        lines = self.rfile.readline()
+    def receive(self) -> Tuple[str, str, str, Dict]:
+        start_line = self.read_line()
 
-    def send(self):
-        self.wfile.write()
+        if not start_line:
+            return
+
+        method, target, version = start_line.split(' ')
+
+        if not method or not target or not version:
+            return
+
+        headers = {}
+
+        while True:
+            header_line = self.read_line()
+
+            if not header_line:
+                break
+
+            key, value = header_line.split(':', maxsplit=1)
+
+            headers[key.strip()] = value.strip()
+
+        return method, target, version, headers
+
+    def send(self, status_code: int = 200, status_text: str = 'OK') -> None:
+        self.write_line(f'HTTP/1.1 {status_code} {status_text}')
+        self.write_line('')
 
     def handle(self) -> None:
         request = self.receive()
 
         self.send()
+
+    def read_line(self) -> Optional[str]:
+        line = self.rfile.readline().decode('utf-8')
+
+        return line.strip() if line else None
+
+    def write_line(self, line: str) -> None:
+        self.wfile.write(f'{line}\r\n'.encode())
